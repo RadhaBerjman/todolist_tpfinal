518cd089deaab6aacbf389588831431cbc345b22
Radha Yemayá <radhaberjmangmail.com>
2025-12-16 16:03:13 +0100

    bulma css y estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import "./styles/global.css";
+import 'bulma/css/bulma.min.css';
+import './styles/custom.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Todo List App</h1>
+      <section className="section">
+        <div className="container">
+          <h1 className="title is-1 has-text-centered has-text-primary">
+            📝 My To do List
+            </h1>
+            <p className="subtitle has-text-centered">
+              ¡Organiza tus tareas diarias de manera fácil y rápida!
+            </p>
+            {/* Mi componente de lista de tareas */}
+            <div className="box">
+              {/* Componente de la lista de tareas */}
+              <p className="has-text-centered"> 
+                
+              </p>
+            </div>
+        </div>
+      </section>
     </div>
   );
 }
